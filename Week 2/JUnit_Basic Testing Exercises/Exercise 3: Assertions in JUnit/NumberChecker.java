bd39590bfae145f60df29d617e5ad516a64984e0
KasthuriRam <158154680+KasthuriRam@users.noreply.github.com>
2025-06-29 22:08:44 +0530

    NumberChecker.java

---
+++ Week 2/JUnit_Basic Testing Exercises/Exercise 3: Assertions in JUnit/NumberChecker.java
@@ -0,0 +1,13 @@
+public class NumberChecker {
+    public String checkNumber(int number) {
+        if (number > 0) {
+            return "Positive";
+        }
+        else if (number < 0) {
+            return "Negative";
+        } 
+        else {
+            return "Zero";
+        }
+    }
+}
