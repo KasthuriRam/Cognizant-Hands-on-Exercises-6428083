73bf16c378b568989b74e05aa948cd04cd5853f1
KasthuriRam <158154680+KasthuriRam@users.noreply.github.com>
2025-06-29 22:23:42 +0530

    MockExternalApi.java

---
+++ Week 2/Mockito exercises/Exercise 2: Verifying Interactions/MockExternalApi.java
@@ -0,0 +1,12 @@
+public class MockExternalApi implements ExternalApi {
+    boolean getDataCalled = false;
+    @Override
+    public void getData() {
+        getDataCalled = true;
+        System.out.println("getData() was called");
+    }
+
+    public boolean wasGetDataCalled() {
+        return getDataCalled;
+    }
+}
